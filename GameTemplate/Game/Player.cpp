68e339dcfbac9984ab14cbe92692c3e3da32070a
kisigami <kbc21b12@stu.kawahara.ac.jp>
2022-06-06 21:05:38 +0900

    プレイヤーの待機、走りアニメーション

--- GameTemplate/Game/Player.cpp
+++ GameTemplate/Game/Player.cpp
@@ -7,16 +7,31 @@ namespace
 	const float CHARACON_RADIUS = 20.0f;      //キャラコンの半径
 	const float CHARACON_HEIGHT = 50.0f;      //キャラコンの高さ
 	const float MOVE_SPEED = 100.0f;          //移動速度
-	const float MINIMUMVALUE = 0.001f;        //移動速度の最低値
+	const float MINIMUMVALUE = 0.00001f;        //移動速度の最低値
+}
+
+void Player::InitAnimation()
+{
+	//アニメーションクリップをロードする
+	m_animClips[enAnimClip_Idle].Load("Assets/animData/player/idle.tka");
+	m_animClips[enAnimClip_Idle].SetLoopFlag(true);
+	m_animClips[enAnimClip_Run].Load("Assets/animData/player/run.tka");
+	m_animClips[enAnimClip_Run].SetLoopFlag(true);
 }
 
 bool Player::Start()
 {
+	//アニメーション初期化
+	InitAnimation();
+
 	//モデルの読み込み
-	m_modelRender.Init("Assets/modelData/Player/Demo.tkm");
+	m_modelRender.Init("Assets/modelData/Player/Demo.tkm", m_animClips, enAnimClip_Num);
+
+	//更新
 	m_modelRender.SetPosition(m_position);
 	m_modelRender.Update();
 
+	//キャラコンを初期化
 	m_charaCon.Init(CHARACON_RADIUS,CHARACON_HEIGHT, m_position);
 
 	return true;
@@ -28,6 +43,10 @@ void Player::Update()
 	Rotation();
 	//移動処理
 	Move();
+	//アニメーションの再生
+	PlayAnimation();
+	//各ステートの遷移処理
+	ManageState();
 
 	//モデルの更新
 	m_modelRender.Update();
@@ -37,7 +56,7 @@ void Player::Update()
 
 void Player::Rotation()
 {
-	//コントローラーの入力がなかったら
+	//移動速度がなかったら
 	if (fabsf(m_moveSpeed.x) < MINIMUMVALUE
 		&& fabsf(m_moveSpeed.z) < MINIMUMVALUE)
 	{
@@ -76,6 +95,68 @@ void Player::Move()
 	//座標の更新
 	Vector3 modelPosition = m_position;
 	m_modelRender.SetPosition(modelPosition);
+}
+
+void Player::PlayAnimation()
+{
+	switch (m_playerState)
+	{
+		//待機ステートの時
+	case Player::enPlayerState_Idle:
+		m_modelRender.PlayAnimation(enAnimClip_Idle,0.3f);
+		m_modelRender.SetAnimationSpeed(1.0f);
+		break;
+		//走りステートの時
+	case Player::enPlayerState_Run:
+		m_modelRender.PlayAnimation(enAnimClip_Run, 0.1f);
+		m_modelRender.SetAnimationSpeed(1.0f);
+		break;
+	}
+}
+
+void Player::ManageState()
+{
+	switch (m_playerState)
+	{
+		//待機ステートの時
+	case Player::enPlayerState_Idle:
+		ProcessIdleStateTransition();
+		break;
+		//走りステートの時
+	case Player::enPlayerState_Run:
+		ProcessRunStateTransition();
+		break;
+	}
+}
+
+void Player::ProcessCommonStateTransition()
+{
+	//移動速度があったら
+	if (fabsf(m_moveSpeed.x) >= MINIMUMVALUE ||
+		fabsf(m_moveSpeed.z) >= MINIMUMVALUE)
+	{
+		//走りステートに移行する
+		m_playerState = enPlayerState_Run;
+		return;
+	}
+	//移動速度がなかったら
+	else
+	{
+		//待機ステートに移行する
+		m_playerState = enPlayerState_Idle;
+	}
+}
+
+void Player::ProcessIdleStateTransition()
+{
+	//共通のステートの遷移処理
+	ProcessCommonStateTransition();
+}
+
+void Player::ProcessRunStateTransition()
+{
+	//共通のステートの遷移処理
+	ProcessCommonStateTransition();
 }
 
 void Player::Render(RenderContext& rc)
--- GameTemplate/Game/Player.h
+++ GameTemplate/Game/Player.h
@@ -3,11 +3,24 @@
 class Player:public IGameObject
 {
 public:
+	/// <summary>
+	/// プレイヤーステート
+	/// </summary>
+	enum EnPlayerState
+	{
+		enPlayerState_Idle,   //待機ステート
+		enPlayerState_Run,    //走りステート
+	};
+
 	bool Start();
 	void Update();
 	void Render(RenderContext& rc);
 
 private:
+	/// <summary>
+	/// アニメーション初期化
+	/// </summary>
+	void InitAnimation();
 	/// <summary>
 	/// 回転処理
 	/// </summary>
@@ -16,14 +29,44 @@ private:
 	/// 移動処理
 	/// </summary>
 	void Move();
+	/// <summary>
+	/// アニメーションの再生
+	/// </summary>
+	void PlayAnimation();
+	/// <summary>
+	/// 各ステートの遷移処理
+	/// </summary>
+	void ManageState();
+	/// <summary>
+	/// 共通のステートの遷移処理
+	/// </summary>
+	void ProcessCommonStateTransition();
+	/// <summary>
+	/// 待機ステートの遷移処理
+	/// </summary>
+	void ProcessIdleStateTransition();
+	/// <summary>
+	/// 走りステートの遷移処理
+	/// </summary>
+	void ProcessRunStateTransition();
 
-	ModelRender          m_modelRender;                 //モデルレンダー
-	CharacterController  m_charaCon;                    //キャラクターコントローラー
-	Vector3              m_position;                    //座標
-	Vector3              m_scale;                       //大きさ
-	Vector3              m_moveSpeed;                   //移動速度
-	Vector3              m_forward = Vector3::AxisZ;    //プレイヤーの前ベクトル
-	Quaternion           m_rotation;                    //回転
+	//アニメーションクリップの番号を表す
+	enum EnAnimationClip
+	{
+		enAnimClip_Idle,   //待機アニメーション
+		enAnimClip_Run,    //走りアニメーション
+		enAnimClip_Num     //アニメーションの数
+	};
 
+	EnPlayerState        m_playerState = enPlayerState_Idle;     //プレイヤーステート
+	Animation            m_animation;                            //アニメーション
+	AnimationClip        m_animClips[enAnimClip_Num];           //アニメーションクリップ
+	ModelRender          m_modelRender;                          //モデルレンダー
+	CharacterController  m_charaCon;                             //キャラクターコントローラー
+	Vector3              m_position;                             //座標
+	Vector3              m_scale;                                //大きさ
+	Vector3              m_moveSpeed;                            //移動速度
+	Vector3              m_forward = Vector3::AxisZ;             //プレイヤーの前ベクトル
+	Quaternion           m_rotation;                             //回転
 };
 
